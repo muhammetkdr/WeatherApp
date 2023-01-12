33676b0156775b72f4e72a49f230a1674cfac05b
Muhammet KDR <mskdr1453@gmail.com>
2023-01-12 17:47:37 +0300

    placeholder/error drawable added

--- app/src/main/java/com/muhammetkdr/weatherapp/common/utils/BindingAdapters.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/utils/BindingAdapters.kt
@@ -3,8 +3,12 @@ package com.muhammetkdr.weatherapp.common.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.muhammetkdr.weatherapp.R
 
 @BindingAdapter("setImage")
 fun ImageView.setImage(imageUrl: String?) {
-    Glide.with(this).load(imageUrl).into(this)
+    Glide.with(this).load(imageUrl)
+        .placeholder(R.drawable.ic_downloading)
+        .error(R.drawable.ic_error)
+        .into(this)
 }
