a292ca6b54dcfdad9c23a6f71f04d8c82f3cdcfb
Muhammet KDR <mskdr1453@gmail.com>
2023-01-15 16:55:39 +0300

    weather api service

--- app/src/main/java/com/muhammetkdr/weatherapp/data/WeatherAPIService.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/data/WeatherAPIService.kt
@@ -3,15 +3,10 @@ package com.muhammetkdr.weatherapp.data
 import com.muhammetkdr.weatherapp.BuildConfig
 import com.muhammetkdr.weatherapp.common.utils.Const.Companion.LANGUAGE
 import com.muhammetkdr.weatherapp.common.utils.Const.Companion.UNITS
-import com.muhammetkdr.weatherapp.common.utils.Resource
 import com.muhammetkdr.weatherapp.model.WeatherResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-//  https://api.openweathermap.org/data/3.0/weather?&units=metric&lat={lat}&lon={lon}&appid={API key}
-//  https://api.openweathermap.org/data/2.5/weather?lat={40}&lon={40}&appid=6f6f86438feb65a6bcaa6ee2aa15fe3b
-//  https://api.openweathermap.org/data/3.0/weather?&units=metric&lat={39.922312}&lon={32.8578795}&appid={6f6f86438feb65a6bcaa6ee2aa15fe3b}
 
 interface WeatherAPIService {
 
