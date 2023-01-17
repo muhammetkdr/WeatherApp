097df5bd81aa9feea514081d428a8cc88dc0d685
Muhammet KDR <mskdr1453@gmail.com>
2023-01-17 23:06:15 +0300

    forecast request added

--- app/src/main/java/com/muhammetkdr/weatherapp/data/WeatherAPIService.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/data/WeatherAPIService.kt
@@ -2,8 +2,9 @@ package com.muhammetkdr.weatherapp.data
 
 import com.muhammetkdr.weatherapp.BuildConfig
 import com.muhammetkdr.weatherapp.common.utils.Const.Companion.LANGUAGE
-import com.muhammetkdr.weatherapp.common.utils.Const.Companion.UNITS
-import com.muhammetkdr.weatherapp.model.WeatherResponse
+import com.muhammetkdr.weatherapp.common.utils.Const.Companion.METRIC
+import com.muhammetkdr.weatherapp.model.current.WeatherResponse
+import com.muhammetkdr.weatherapp.model.forecast.ForecastResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,11 +13,20 @@ interface WeatherAPIService {
 
     @GET("data/2.5/weather")
     suspend fun getCurrentWeather(
-        @Query("units") unit: String = UNITS,
+        @Query("units") unit: String = METRIC,
         @Query("lat") latitude : String,
         @Query("lon") longitude : String,
         @Query("lang") language : String = LANGUAGE,
         @Query("appid") apiKey:String = BuildConfig.API_KEY
     ) : Response<WeatherResponse>
+
+    @GET("data/2.5/forecast")
+    suspend fun getForecastWeather(
+        @Query("units") unit: String = METRIC,
+        @Query("lat") latitude : String,
+        @Query("lon") longitude : String,
+        @Query("lang") language : String = LANGUAGE,
+        @Query("appid") apiKey:String = BuildConfig.API_KEY
+    ) : Response<ForecastResponse>
 
 }
