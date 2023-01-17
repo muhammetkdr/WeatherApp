f2c5470ba2d247f4a2e6fb87a05702c47aa92981
Muhammet KDR <mskdr1453@gmail.com>
2023-01-17 23:08:06 +0300

    review

--- app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Resource.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Resource.kt
@@ -2,6 +2,6 @@ package com.muhammetkdr.weatherapp.common.utils
 
 sealed class Resource<out T : Any> {
     data class Success<out T : Any>(val data: T) : Resource<T>()
-    data class Error<out T: Any>(val message: String) : Resource<T>()
+    data class Error<out T : Any>(val error: String) : Resource<T>()
     object Loading : Resource<Nothing>()
 }
