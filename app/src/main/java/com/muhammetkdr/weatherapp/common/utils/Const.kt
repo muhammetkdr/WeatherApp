8f1d6a42421063339666b41074a49ac3cad5d12e
Muhammet KDR <mskdr1453@gmail.com>
2023-01-17 23:11:49 +0300

    request durations added

--- app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Const.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Const.kt
@@ -2,8 +2,11 @@ package com.muhammetkdr.weatherapp.common.utils
 
 class Const {
     companion object{
-        const val UNITS = "metric"
+        const val METRIC = "metric"
         const val LANGUAGE = "tr"
         const val REQUEST_CODE_LOCATION_PERMISSION = 1
+        const val LOCATION_REQUEST_DURATION = 600000L
+        const val LOCATION_REQUEST_MIN_DURATION = 2000L
+        const val LOCATION_REQUEST_MAX_DURATION = 4000L
     }
 }
