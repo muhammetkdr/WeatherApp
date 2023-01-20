8faa4c7bf6fec8864e24a0fb5ee062c0f718c377
Muhammet KDR <mskdr1453@gmail.com>
2023-01-21 00:36:34 +0300

    Destructuring declarations added

---
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/CalendarExtension.kt
@@ -0,0 +1,9 @@
+package com.muhammetkdr.weatherapp.common.extensions
+
+import java.util.*
+
+
+//Destructuring declarations
+operator fun Calendar.component1() = get(Calendar.DATE)
+operator fun Calendar.component2() = get(Calendar.MONTH)+1
+operator fun Calendar.component3() = get(Calendar.YEAR)
