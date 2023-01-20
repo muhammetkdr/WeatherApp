ddc58f1613455211fde3fd81ac2a85f5b43ad458
Muhammet KDR <mskdr1453@gmail.com>
2023-01-21 00:38:11 +0300

    binding adapter for rv. added

--- app/src/main/java/com/muhammetkdr/weatherapp/common/utils/BindingAdapters.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/utils/BindingAdapters.kt
@@ -3,11 +3,21 @@ package com.muhammetkdr.weatherapp.common.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.muhammetkdr.weatherapp.BuildConfig
 import com.muhammetkdr.weatherapp.R
+import com.muhammetkdr.weatherapp.common.utils.Const.Companion.IMAGE_RESOLUTION_SIZE
 
 @BindingAdapter("setImage")
 fun ImageView.setImage(imageUrl: String?) {
     Glide.with(this).load(imageUrl)
+        .placeholder(R.drawable.ic_downloading)
+        .error(R.drawable.ic_error)
+        .into(this)
+}
+
+@BindingAdapter("setWeatherImage")
+fun ImageView.setWeatherImage(weatherCondition:String?) {
+    Glide.with(this).load(BuildConfig.IMAGE_URL + weatherCondition + IMAGE_RESOLUTION_SIZE)
         .placeholder(R.drawable.ic_downloading)
         .error(R.drawable.ic_error)
         .into(this)
