aece96d6c06398e5981392e5a5effdea9ae4558d
Muhammet KDR <mskdr1453@gmail.com>
2023-01-23 22:26:10 +0300

    easy permission lib. deleted, now permission requests have been provided by android's own features

--- app/src/main/java/com/muhammetkdr/weatherapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/ui/home/HomeFragment.kt
@@ -4,50 +4,68 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.muhammetkdr.weatherapp.R
 import com.muhammetkdr.weatherapp.base.BaseFragment
 import com.muhammetkdr.weatherapp.common.extensions.*
+import com.muhammetkdr.weatherapp.common.utils.Const.Companion.LOCATION_PERMISSION_REQUEST_CODE_ZERO
 import com.muhammetkdr.weatherapp.common.utils.Const.Companion.LOCATION_REQUEST_DURATION
-import com.muhammetkdr.weatherapp.common.utils.Const.Companion.REQUEST_CODE_LOCATION_PERMISSION
 import com.muhammetkdr.weatherapp.common.utils.Resource
-import com.muhammetkdr.weatherapp.common.utils.setImage
 import com.muhammetkdr.weatherapp.databinding.FragmentHomeBinding
 import com.muhammetkdr.weatherapp.location.DefaultLocationClient
-import com.vmadalin.easypermissions.EasyPermissions
-import com.vmadalin.easypermissions.dialogs.SettingsDialog
+import com.muhammetkdr.weatherapp.model.forecast.WeatherList
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
     FragmentHomeBinding::inflate
-), EasyPermissions.PermissionCallbacks {
+){
     override val viewModel by viewModels<HomeViewModel>()
+
+    // todo @Inject adapter.. i couldn't find how to do it yet..
+    private val adapter: HomeWeatherAdapter by lazy { HomeWeatherAdapter(::navigateWeatherPage) }
 
     @Inject
     lateinit var defaultLocationClient: DefaultLocationClient
 
+    @Inject
+    lateinit var calendar: Calendar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestPermission()
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(), arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ), LOCATION_PERMISSION_REQUEST_CODE_ZERO
+        )
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        requestLocationPermission()
-        onPermissionRequestResult()
+        observeCurrentWeatherData()
+        observeForecastWeatherData()
 
         initDataBinding()
         getLocation()
-        initUi()
     }
 
     @SuppressLint("MissingPermission")
     private fun getLocation() = lifecycleScope.launchWhenStarted {
         try {
             defaultLocationClient.getLocationUpdates(LOCATION_REQUEST_DURATION).collect {
-                viewModel.getWeather(it.latitude, it.longitude)
+                viewModel.getCurrentWeather(it.latitude, it.longitude)
+                viewModel.getForecastWeather(it.latitude, it.longitude)
             }
         } catch (e: Exception) {
             requireView().showSnackbar(
@@ -57,16 +75,22 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
         }
     }
 
-    private fun initUi() {
-        viewModel.weatherList.observe(viewLifecycleOwner) { Resource ->
+    private fun observeCurrentWeatherData() = lifecycleScope.launchWhenStarted {
+        viewModel.currentWeather.collect { Resource ->
             when (Resource) {
                 is Resource.Success -> {
                     Resource.data.let {
                         binding.homeProgressbar.gone()
+
+                        val (date, month, year) = calendar
+
+                        binding.customToolBar.updateTitle("$date, $month, $year")
                         binding.containerForecast.weatherResponse = it
 //                        binding.containerForecast.imgViewCustomHome.setImageResource(R.drawable.background)
                         binding.textView.text =
-                            it.name.toString() + "\n" + it.main?.temp?.toString() + "\n" + it.weather?.get(0)?.main + "\n" + it.weather?.get(0)?.description?.capitalizeWords()
+                            it.name.toString() + "\n" + it.main?.temp?.toString() + "\n" + it.weather?.get(
+                                0
+                            )?.main + "\n" + it.weather?.get(0)?.description?.capitalizeWords()
                     }
                 }
                 is Resource.Error -> {
@@ -80,53 +104,42 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
         }
     }
 
-    private fun initDataBinding() {
-        binding.lifecycleOwner = viewLifecycleOwner
-    }
+    private fun observeForecastWeatherData() = lifecycleScope.launchWhenStarted {
+        viewModel.forecastWeatherList.collect { Resource ->
+            when (Resource) {
+                is Resource.Success -> {
+                    Resource.data.let { forecastWeatherResponse ->
+                        binding.homeProgressbar.gone()
+                        adapter.submitList(forecastWeatherResponse.list)
+                        adapter.currentList.forEach {
 
-    private fun requestLocationPermission() {
-        if (requireContext().checkLocationPermission()) {
-            return
-        }
-        EasyPermissions.requestPermissions(
-            this,
-            requireContext().resources.getString(R.string.permission_denied_rationale),
-            REQUEST_CODE_LOCATION_PERMISSION,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-    }
-
-    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
-        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            SettingsDialog.Builder(requireContext()).build().show()
-        } else {
-            requestLocationPermission()
-        }
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
-        Toast.makeText(
-            requireContext(),
-            requireContext().resources.getString(R.string.permission_granted),
-            Toast.LENGTH_SHORT
-        ).show()
-    }
-
-    private fun onPermissionRequestResult() =
-        object : ActivityCompat.OnRequestPermissionsResultCallback {
-            override fun onRequestPermissionsResult(
-                requestCode: Int,
-                permissions: Array<out String>,
-                grantResults: IntArray
-            ) {
-                EasyPermissions.onRequestPermissionsResult(
-                    requestCode,
-                    permissions,
-                    grantResults,
-                    this
-                )
+                        }
+                    }
+                }
+                is Resource.Error -> {
+                    binding.homeProgressbar.visible()
+                    requireView().showSnackbar(Resource.error)
+                }
+                is Resource.Loading -> {
+                    binding.homeProgressbar.visible()
+                }
             }
         }
+    }
+
+    private fun initDataBinding() = with(binding) {
+        rvWeatherHome.adapter = adapter
+        rvWeatherHome.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        lifecycleOwner = viewLifecycleOwner
+        containerForecast.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun navigateWeatherPage(data: WeatherList) {
+
+        //  findNavController().popBackStack()
+    }
+
 }
 
