6609fae368b759661a99504d60b8aaa1360a69cf
Muhammet KDR <mskdr1453@gmail.com>
2023-01-23 22:24:21 +0300

    context extension changed

--- app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/ContextExtensions.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/ContextExtensions.kt
@@ -2,12 +2,14 @@ package com.muhammetkdr.weatherapp.common.extensions
 
 import android.Manifest
 import android.content.Context
-import com.vmadalin.easypermissions.EasyPermissions
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 
-fun Context.checkLocationPermission(): Boolean =
-    EasyPermissions.hasPermissions(
-        this,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    )
+fun Context.hasLocationPermission(): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this, Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+        this, Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+}
 
