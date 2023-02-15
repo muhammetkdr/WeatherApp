ea2f48a32e2ab71b9feff4e57f8295e62044724e
Muhammet KDR <mskdr1453@gmail.com>
2023-02-15 18:04:42 +0300

    Weather useCaseModule added

---
+++ app/src/main/java/com/muhammetkdr/weatherapp/di/UseCaseModule.kt
@@ -0,0 +1,19 @@
+package com.muhammetkdr.weatherapp.di
+
+import com.muhammetkdr.weatherapp.domain.usecase.CurrentWeatherUseCase
+import com.muhammetkdr.weatherapp.domain.usecase.CurrentWeatherUseCaseImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class UseCaseModule {
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindCurrentWeatherUseCase(currentWeatherUseCaseImpl: CurrentWeatherUseCaseImpl): CurrentWeatherUseCase
+
+}
