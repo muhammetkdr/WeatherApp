e993e3a8722cae69bccd2b23fe779632c2e0cc68
Muhammet KDR <mskdr1453@gmail.com>
2023-02-23 20:47:10 +0300

    data provider changed with flow

--- app/src/main/java/com/muhammetkdr/weatherapp/domain/usecase/CurrentWeatherUseCaseImpl.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/domain/usecase/CurrentWeatherUseCaseImpl.kt
@@ -4,11 +4,8 @@ import com.muhammetkdr.weatherapp.common.utils.Resource
 import com.muhammetkdr.weatherapp.domain.entity.CurrentWeatherEntity
 import com.muhammetkdr.weatherapp.domain.repository.WeatherRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class CurrentWeatherUseCaseImpl @Inject constructor (private val weatherRepository: WeatherRepository) :CurrentWeatherUseCase{
-    override fun invoke(lat: String, long:String): Flow<Resource<CurrentWeatherEntity>> = flow {
-        emit(weatherRepository.getCurrentWeather(lat, long))
-    }
+    override fun invoke(lat: String, long:String): Flow<Resource<CurrentWeatherEntity>> = weatherRepository.getCurrentWeather(lat,long)
 }
