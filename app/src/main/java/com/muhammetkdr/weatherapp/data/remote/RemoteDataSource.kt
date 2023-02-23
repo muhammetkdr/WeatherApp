8dea19bc1502a05d6718e68d70d40f72dad492eb
Muhammet KDR <mskdr1453@gmail.com>
2023-02-23 20:48:10 +0300

    data provider changed with flow

--- app/src/main/java/com/muhammetkdr/weatherapp/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/data/remote/RemoteDataSource.kt
@@ -3,8 +3,9 @@ package com.muhammetkdr.weatherapp.data.remote
 import com.muhammetkdr.weatherapp.common.utils.Resource
 import com.muhammetkdr.weatherapp.data.dto.current.WeatherResponse
 import com.muhammetkdr.weatherapp.data.dto.forecast.ForecastResponse
+import kotlinx.coroutines.flow.Flow
 
 interface RemoteDataSource {
-    suspend fun getCurrentWeather(lat: String, long:String): Resource<WeatherResponse>
-    suspend fun getForecastWeather(lat: String, long:String): Resource<ForecastResponse>
+    fun getCurrentWeather(lat: String, long:String): Flow<Resource<WeatherResponse>>
+    fun getForecastWeather(lat: String, long:String): Flow<Resource<ForecastResponse>>
 }
--- app/src/main/java/com/muhammetkdr/weatherapp/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/data/remote/RemoteDataSourceImpl.kt
@@ -4,51 +4,47 @@ import com.muhammetkdr.weatherapp.common.utils.Resource
 import com.muhammetkdr.weatherapp.data.api.WeatherAPIService
 import com.muhammetkdr.weatherapp.data.dto.current.WeatherResponse
 import com.muhammetkdr.weatherapp.data.dto.forecast.ForecastResponse
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class RemoteDataSourceImpl @Inject constructor(
     private val api: WeatherAPIService,
-    private val ioDispatcher:CoroutineContext
 ) : RemoteDataSource {
 
-   override suspend fun getCurrentWeather(lat: String, long:String): Resource<WeatherResponse> = withContext(ioDispatcher){
-        return@withContext try {
-            Resource.Loading
+    override fun getCurrentWeather(lat: String, long: String): Flow<Resource<WeatherResponse>> = flow {
+        try {
+            emit(Resource.Loading)
             val response = api.getCurrentWeather(latitude = lat, longitude = long)
-            delay(200)
             if (response.isSuccessful) {
                 response.body()?.let {
-                    return@let Resource.Success(it)
-                } ?: Resource.Error(NO_DATA)
+                    emit(Resource.Success(it))
+                }
             } else {
-                Resource.Error(NO_DATA)
+                emit(Resource.Error(NO_DATA))
             }
         } catch (e: Exception) {
-            Resource.Error(e.localizedMessage ?: SOMETHING_BAD_HAPPENED)
+            emit(Resource.Error(e.localizedMessage ?: SOMETHING_BAD_HAPPENED))
         }
     }
 
-    override suspend fun getForecastWeather(lat: String, long: String): Resource<ForecastResponse> = withContext(ioDispatcher){
-        return@withContext try {
-            Resource.Loading
+    override fun getForecastWeather(lat: String, long: String): Flow<Resource<ForecastResponse>> = flow {
+        try {
+            emit(Resource.Loading)
             val response = api.getForecastWeather(latitude = lat, longitude = long)
-            delay(200)
             if (response.isSuccessful) {
                 response.body()?.let {
-                    return@let Resource.Success(it)
-                } ?: Resource.Error(NO_DATA)
+                    emit(Resource.Success(it))
+                }
             } else {
-                Resource.Error(NO_DATA)
+                emit(Resource.Error(NO_DATA))
             }
         } catch (e: Exception) {
-            Resource.Error(e.localizedMessage ?: SOMETHING_BAD_HAPPENED)
+            emit(Resource.Error(e.localizedMessage ?: SOMETHING_BAD_HAPPENED))
         }
     }
 
-    companion object{
+    companion object {
         private const val NO_DATA = "No Data!"
         private const val SOMETHING_BAD_HAPPENED = "Something bad happened.."
     }
