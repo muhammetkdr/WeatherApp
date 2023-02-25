b40885b481f6055026bd9f90a9d0a436e039ed71
Muhammet KDR <mskdr1453@gmail.com>
2023-02-25 23:24:42 +0300

    package changed

--- app/src/main/java/com/muhammetkdr/weatherapp/domain/usecase/CurrentWeatherUseCaseImpl.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/domain/usecase/CurrentWeatherUseCaseImpl.kt
@@ -1,7 +1,7 @@
 package com.muhammetkdr.weatherapp.domain.usecase
 
 import com.muhammetkdr.weatherapp.common.utils.Resource
-import com.muhammetkdr.weatherapp.domain.entity.CurrentWeatherEntity
+import com.muhammetkdr.weatherapp.domain.entity.currentweather.CurrentWeatherEntity
 import com.muhammetkdr.weatherapp.domain.repository.WeatherRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
