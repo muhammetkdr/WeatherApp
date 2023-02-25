addd9b9140720f03be0272b20bda3f4f7af09ab7
Muhammet KDR <mskdr1453@gmail.com>
2023-02-25 23:21:14 +0300

    logger and callender provider added

--- app/src/main/java/com/muhammetkdr/weatherapp/di/AppModule.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/di/AppModule.kt
@@ -1,14 +1,25 @@
 package com.muhammetkdr.weatherapp.di
 
+import com.muhammetkdr.weatherapp.common.logger.Logger
+import com.muhammetkdr.weatherapp.common.logger.LoggerImpl
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.FragmentComponent
+import dagger.hilt.android.scopes.FragmentScoped
+import java.util.Calendar
+
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(FragmentComponent::class)
 object AppModule {
 
+    @Provides
+    @FragmentScoped
+    fun provideLogger() :Logger = LoggerImpl()
 
-
+    @Provides
+    @FragmentScoped
+    fun provideCallender():Calendar = Calendar.getInstance()
 
 }
