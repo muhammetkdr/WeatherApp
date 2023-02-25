c854d2fdd14f737b4ff62f5796df3ba4e074226d
Muhammet KDR <mskdr1453@gmail.com>
2023-02-25 23:24:55 +0300

    package changed

--- app/src/main/java/com/muhammetkdr/weatherapp/ui/home/uidata/CurrentWeatherUiModelMapper.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/ui/home/uidata/CurrentWeatherUiModelMapper.kt
@@ -1,17 +1,18 @@
 package com.muhammetkdr.weatherapp.ui.home.uidata
 
-import com.muhammetkdr.weatherapp.domain.entity.CurrentWeatherEntity
+import com.muhammetkdr.weatherapp.domain.entity.currentweather.CurrentWeatherEntity
 import com.muhammetkdr.weatherapp.domain.mapper.WeatherMapper
 
 import javax.inject.Inject
 
 class CurrentWeatherUiModelMapper @Inject constructor() :
     WeatherMapper<CurrentWeatherEntity, CurrentWeatherUiModel> {
-    override fun map(input: CurrentWeatherEntity): CurrentWeatherUiModel = with(input) {
-        CurrentWeatherUiModel(
-            this.name,
-            this.sys,
-            this.main,
-            this.weather)
+    override fun map(input: CurrentWeatherEntity): CurrentWeatherUiModel {
+        return CurrentWeatherUiModel(
+            name = input.name,
+            sys = input.sys,
+            main = input.main,
+            weather = input.weather
+        )
     }
 }
