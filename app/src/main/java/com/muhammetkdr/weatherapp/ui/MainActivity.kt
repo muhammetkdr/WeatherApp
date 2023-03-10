e76f0c109dd01e977ad271e89105f3d3f9943098
Muhammet KDR <mskdr1453@gmail.com>
2023-03-10 16:20:35 +0300

    Base Activity inheritance added

--- app/src/main/java/com/muhammetkdr/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/ui/MainActivity.kt
@@ -1,55 +1,10 @@
 package com.muhammetkdr.weatherapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.muhammetkdr.weatherapp.common.logger.LoggerImpl
+import com.muhammetkdr.weatherapp.base.BaseActivity
 import com.muhammetkdr.weatherapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
-    @Inject
-    lateinit var logger: LoggerImpl
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        logger.log("onCreate")
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-    }
-
-    override fun onStart() {
-        logger.log("onStart")
-        super.onStart()
-    }
-    override fun onResume() {
-        logger.log("onResume")
-        super.onResume()
-    }
-
-    override fun onRestart() {
-        logger.log("onRestart")
-        super.onRestart()
-    }
-
-    override fun onPause() {
-        logger.log("onPause")
-        super.onPause()
-    }
-
-    override fun onStop() {
-        logger.log("onStop")
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        logger.log("onDestroy")
-        super.onDestroy()
-    }
-
-}
+class MainActivity : BaseActivity<ActivityMainBinding>({ inflater ->
+    ActivityMainBinding.inflate(inflater)
+})
