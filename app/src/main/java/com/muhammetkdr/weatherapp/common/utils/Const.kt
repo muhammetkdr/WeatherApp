f19722afae5dd2fc32beac770fd2d541aac67c1d
Muhammet KDR <mskdr1453@gmail.com>
2023-03-11 22:28:16 +0300

    unused value deleted

--- app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Const.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Const.kt
@@ -4,7 +4,6 @@ class Const {
     companion object{
         const val METRIC = "metric"
         const val LANGUAGE = "tr"
-        const val LOCATION_PERMISSION_REQUEST_CODE_ZERO = 0
 
         const val LOCATION_REQUEST_DURATION = 600000L
         const val LOCATION_REQUEST_MIN_DURATION = 2000L
