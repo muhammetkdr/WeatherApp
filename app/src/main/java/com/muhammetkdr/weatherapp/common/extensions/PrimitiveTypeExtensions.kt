0070fbe7bc5ed8f5a79160c1b3a36f5f5abef7c3
Muhammet KDR <mskdr1453@gmail.com>
2023-03-15 00:10:26 +0300

    double to string ext

--- app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/PrimitiveTypeExtensions.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/PrimitiveTypeExtensions.kt
@@ -6,3 +6,7 @@ fun Float?.orZero() = this ?: 0.0f
 fun Int?.orZero() = this ?: 0
 
 fun Double?.orZero() = this ?: 0.0
+
+fun Double?.nullToString(): String {
+    return this?.toString() ?: "null"
+}
