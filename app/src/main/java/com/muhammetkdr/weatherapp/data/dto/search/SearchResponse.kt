5a5883ba4d1fbb5a27c8ab053c77739c4bc2ff91
Muhammet KDR <mskdr1453@gmail.com>
2023-03-15 00:06:58 +0300

    Search Response dto created

---
+++ app/src/main/java/com/muhammetkdr/weatherapp/data/dto/search/SearchResponse.kt
@@ -0,0 +1,34 @@
+package com.muhammetkdr.weatherapp.data.dto.search
+
+
+import com.google.gson.annotations.SerializedName
+import com.muhammetkdr.weatherapp.data.dto.current.*
+
+data class SearchResponse(
+    @SerializedName("base")
+    val base: String?,
+    @SerializedName("clouds")
+    val clouds: Clouds?,
+    @SerializedName("cod")
+    val cod: Int?,
+    @SerializedName("coord")
+    val coord: Coord?,
+    @SerializedName("dt")
+    val dt: Int?,
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("main")
+    val main: Main?,
+    @SerializedName("name")
+    val name: String?,
+    @SerializedName("sys")
+    val sys: Sys?,
+    @SerializedName("timezone")
+    val timezone: Int?,
+    @SerializedName("visibility")
+    val visibility: Int?,
+    @SerializedName("weather")
+    val weather: List<Weather>?,
+    @SerializedName("wind")
+    val wind: Wind?
+)
