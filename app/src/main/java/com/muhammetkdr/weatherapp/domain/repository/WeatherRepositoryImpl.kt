3520adc80e44af789615218c5f98c1239e0b6f86
Muhammet KDR <mskdr1453@gmail.com>
2023-03-15 23:53:33 +0300

    ioDispatcher added for request

--- app/src/main/java/com/muhammetkdr/weatherapp/domain/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/domain/repository/WeatherRepositoryImpl.kt
@@ -14,35 +14,38 @@ import com.muhammetkdr.weatherapp.data.listmapper.WeatherListMapper
 import com.muhammetkdr.weatherapp.data.mapper.WeatherMapper
 import com.muhammetkdr.weatherapp.domain.entity.searchweather.SearchWeatherEntity
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 class WeatherRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
     private val currentWeatherMapper: WeatherMapper<WeatherResponse, CurrentWeatherEntity>,
     private val forecastWeatherMapper: WeatherMapper<ForecastResponse, ForecastWeatherEntity>,
     private val searchWeatherMapper: WeatherMapper<SearchResponse, SearchWeatherEntity>,
-    private val weatherListMapper: WeatherListMapper<WeatherList, WeatherListEntity>
-) : WeatherRepository{
+    private val weatherListMapper: WeatherListMapper<WeatherList, WeatherListEntity>,
+    private val ioDispatcher: CoroutineContext
+) : WeatherRepository {
 
     override fun getCurrentWeather(lat: String, long: String): Flow<Resource<CurrentWeatherEntity>> =
-        remoteDataSource.getCurrentWeather(lat,long).map{
-              it.mapResource { currentWeatherMapper.map(this)}
-        }
+        remoteDataSource.getCurrentWeather(lat, long).map {
+            it.mapResource { currentWeatherMapper.map(this) }
+        }.flowOn(ioDispatcher)
 
     override fun getForecastWeather(lat: String, long: String): Flow<Resource<ForecastWeatherEntity>> =
-        remoteDataSource.getForecastWeather(lat,long).map {
+        remoteDataSource.getForecastWeather(lat, long).map {
             it.mapResource { forecastWeatherMapper.map(this) }
-    }
+        }.flowOn(ioDispatcher)
 
     override fun getWeatherList(lat: String, long: String): Flow<Resource<List<WeatherListEntity>>> =
-        remoteDataSource.getWeatherList(lat,long).map {
+        remoteDataSource.getWeatherList(lat, long).map {
             it.mapResource { weatherListMapper.map(this) }
-        }
+        }.flowOn(ioDispatcher)
 
     override fun getSearchResponse(cityNameQuery: String): Flow<Resource<SearchWeatherEntity>> =
         remoteDataSource.getSearchResponse(cityNameQuery).map {
             it.mapResource { searchWeatherMapper.map(this) }
-        }
+        }.flowOn(ioDispatcher)
 
 }
