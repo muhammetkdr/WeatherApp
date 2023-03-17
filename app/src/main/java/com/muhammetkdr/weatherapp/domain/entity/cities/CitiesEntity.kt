d1d3efdab0a1daa9b64edf85c5b17c7d2a0a9bfe
Muhammet KDR <mskdr1453@gmail.com>
2023-03-17 17:01:39 +0300

    pacelized for jetpack navigation

--- app/src/main/java/com/muhammetkdr/weatherapp/domain/entity/cities/CitiesEntity.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/domain/entity/cities/CitiesEntity.kt
@@ -1,3 +1,12 @@
 package com.muhammetkdr.weatherapp.domain.entity.cities
 
-data class CitiesEntity(val latitude: String, val longitude: String, val cityName: String, val cityPlateCode : String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class CitiesEntity(
+    val latitude: String,
+    val longitude: String,
+    val cityName: String,
+    val cityPlateCode: String
+) : Parcelable
