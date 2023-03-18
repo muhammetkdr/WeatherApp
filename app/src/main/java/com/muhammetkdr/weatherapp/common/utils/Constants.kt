75b55e7a963ab7aeffcddeceae71d0d1c18d74bf
Muhammet KDR <mskdr1453@gmail.com>
2023-03-18 14:57:58 +0300

    duration changed

--- app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Constants.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/utils/Constants.kt
@@ -4,8 +4,8 @@ object Constants {
         const val METRIC = "metric"
         const val LANGUAGE = "tr"
         const val LOCATION_REQUEST_DURATION = 600000L
-        const val LOCATION_REQUEST_MIN_DURATION = 2000L
-        const val LOCATION_REQUEST_MAX_DURATION = 4000L
+        const val LOCATION_REQUEST_MIN_DURATION = 3000L
+        const val LOCATION_REQUEST_MAX_DURATION = 20000L
         const val IMAGE_RESOLUTION_SIZE = "@4x.png"
 
         const val CITY_BASE_URL = "https://gist.github.com/"
