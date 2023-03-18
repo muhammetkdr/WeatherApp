782cb19728dac8df641064fe7dc2947c1799fb52
Muhammet KDR <mskdr1453@gmail.com>
2023-03-18 18:31:10 +0300

    renamed

--- app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/FloatIntDoubleExt.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/FloatIntDoubleExt.kt
@@ -3,10 +3,6 @@ package com.muhammetkdr.weatherapp.common.extensions
 
 fun Float?.orZero() = this ?: 0.0f
 
-fun Int?.orZero() = this ?: 0
-
-fun Double?.orZero() = this ?: 0.0
-
 fun Double?.nullToString(): String {
     return this?.toString() ?: "null"
 }
