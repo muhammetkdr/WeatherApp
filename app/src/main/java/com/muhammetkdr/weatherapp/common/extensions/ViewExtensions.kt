f79d069b9657feca5f9f162970ddae52b50f0b1c
Muhammet KDR <mskdr1453@gmail.com>
2023-03-23 19:21:30 +0300

    unused ext. deleted

--- app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/ViewExtensions.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/common/extensions/ViewExtensions.kt
@@ -3,14 +3,6 @@ package com.muhammetkdr.weatherapp.common.extensions
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
-fun View.visible() {
-    visibility = View.VISIBLE
-}
-
-fun View.gone() {
-    visibility = View.GONE
-}
-
 fun View.showSnackbar(msg: String) {
     Snackbar.make(this, msg, 10000).show()
 }
