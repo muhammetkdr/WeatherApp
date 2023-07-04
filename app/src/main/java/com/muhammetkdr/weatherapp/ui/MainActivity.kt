c49f8f1468422e9f99294d284726efae25966a53
Muhammet KDR <mskdr1453@gmail.com>
2023-07-05 01:23:17 +0300

    unnecessary baseActivity usage removed.

--- app/src/main/java/com/muhammetkdr/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/ui/MainActivity.kt
@@ -1,8 +1,55 @@
 package com.muhammetkdr.weatherapp.ui
 
-import com.muhammetkdr.weatherapp.base.BaseActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.muhammetkdr.weatherapp.common.logger.Logger
 import com.muhammetkdr.weatherapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    @Inject
+    lateinit var logger: Logger
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        logger.log("onCreate")
+    }
+
+    override fun onStart() {
+        logger.log("onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        logger.log("onResume")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        logger.log("onRestart")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        logger.log("onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        logger.log("onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        logger.log("onDestroy")
+        super.onDestroy()
+    }
+}
