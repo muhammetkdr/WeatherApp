a7d329ca4ecfb0d3ccfb392c72b9097f8df6128f
Muhammet KDR <mskdr1453@gmail.com>
2023-07-26 13:32:58 +0300

    wrong implementation changed

--- app/src/main/java/com/muhammetkdr/weatherapp/di/CoroutineModule.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/di/CoroutineModule.kt
@@ -4,11 +4,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import java.util.*
 import javax.inject.Qualifier
 import javax.inject.Singleton
-import kotlin.coroutines.CoroutineContext
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -17,22 +17,22 @@ object CoroutineModule {
     @Dispatcher(DispatcherType.Io)
     @Provides
     @Singleton
-    fun provideDispatcheIo(): CoroutineContext = Dispatchers.IO
+    fun provideDispatcheIo(): CoroutineDispatcher = Dispatchers.IO
 
     @Dispatcher(DispatcherType.Unconfined)
     @Provides
     @Singleton
-    fun provideDispatcherUncf(): CoroutineContext = Dispatchers.Unconfined
+    fun provideDispatcherUncf(): CoroutineDispatcher = Dispatchers.Unconfined
 
     @Dispatcher(DispatcherType.Default)
     @Provides
     @Singleton
-    fun provideDispatcherDefault(): CoroutineContext = Dispatchers.Default
+    fun provideDispatcherDefault(): CoroutineDispatcher = Dispatchers.Default
 
     @Dispatcher(DispatcherType.Main)
     @Provides
     @Singleton
-    fun provideDispatcherMain(): CoroutineContext = Dispatchers.Main
+    fun provideDispatcherMain(): CoroutineDispatcher = Dispatchers.Main
 
 }
 
