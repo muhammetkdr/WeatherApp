d7f34286452a49da6ba27b3bcc095f574aaf5da3
Muhammet KDR <mskdr1453@gmail.com>
2023-07-26 13:32:31 +0300

    unnecessary ioDispatcher using has been removed

--- app/src/main/java/com/muhammetkdr/weatherapp/data/repository/city/CityRepositoryImpl.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/data/repository/city/CityRepositoryImpl.kt
@@ -5,23 +5,18 @@ import com.muhammetkdr.weatherapp.common.utils.Resource
 import com.muhammetkdr.weatherapp.data.dto.city.CitiesResponse
 import com.muhammetkdr.weatherapp.data.listmapper.ListMapper
 import com.muhammetkdr.weatherapp.data.remote.city.CityRemoteDataSource
-import com.muhammetkdr.weatherapp.di.Dispatcher
-import com.muhammetkdr.weatherapp.di.DispatcherType
 import com.muhammetkdr.weatherapp.domain.entity.cities.CitiesEntity
 import com.muhammetkdr.weatherapp.domain.repository.city.CityRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class CityRepositoryImpl @Inject constructor(
     private val cityRemoteDataSource: CityRemoteDataSource,
-    private val cityListMapper: ListMapper<CitiesResponse, CitiesEntity>,
-    @Dispatcher(DispatcherType.Io) private val ioDispatcher: CoroutineContext
+    private val cityListMapper: ListMapper<CitiesResponse, CitiesEntity>
 ) : CityRepository {
     override fun getAllCities(): Flow<Resource<List<CitiesEntity>>> =
         cityRemoteDataSource.getCityResponse().map {
         it.mapResource { cityListMapper.map(this) }
-    }.flowOn(ioDispatcher)
+    }
 }
--- app/src/main/java/com/muhammetkdr/weatherapp/data/repository/weather/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/muhammetkdr/weatherapp/data/repository/weather/WeatherRepositoryImpl.kt
@@ -6,31 +6,26 @@ import com.muhammetkdr.weatherapp.data.dto.current.WeatherResponse
 import com.muhammetkdr.weatherapp.data.dto.forecast.ForecastResponse
 import com.muhammetkdr.weatherapp.data.mapper.WeatherMapper
 import com.muhammetkdr.weatherapp.data.remote.weather.WeatherRemoteDataSource
-import com.muhammetkdr.weatherapp.di.Dispatcher
-import com.muhammetkdr.weatherapp.di.DispatcherType
 import com.muhammetkdr.weatherapp.domain.entity.currentweather.CurrentWeatherEntity
 import com.muhammetkdr.weatherapp.domain.entity.forecastweather.ForecastWeatherEntity
 import com.muhammetkdr.weatherapp.domain.repository.weather.WeatherRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
 
 class WeatherRepositoryImpl @Inject constructor(
     private val weatherRemoteDataSource: WeatherRemoteDataSource,
     private val currentWeatherMapper: WeatherMapper<WeatherResponse, CurrentWeatherEntity>,
-    private val forecastWeatherMapper: WeatherMapper<ForecastResponse, ForecastWeatherEntity>,
-    @Dispatcher(DispatcherType.Io) private val ioDispatcher: CoroutineContext
+    private val forecastWeatherMapper: WeatherMapper<ForecastResponse, ForecastWeatherEntity>
 ) : WeatherRepository {
 
     override fun getCurrentWeather(lat: String, long: String): Flow<Resource<CurrentWeatherEntity>> =
         weatherRemoteDataSource.getCurrentWeather(lat, long).map {
             it.mapResource { currentWeatherMapper.map(this) }
-        }.flowOn(ioDispatcher)
+        }
 
     override fun getForecastWeather(lat: String, long: String): Flow<Resource<ForecastWeatherEntity>> =
         weatherRemoteDataSource.getForecastWeather(lat, long).map {
             it.mapResource { forecastWeatherMapper.map(this) }
-        }.flowOn(ioDispatcher)
+        }
 }
