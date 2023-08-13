6d1bbb055030521d7bc71e9c47db1e0596b6c3db
Muhammet KDR <mskdr1453@gmail.com>
2023-08-13 18:14:44 +0300

    version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ allprojects{
 }
 
 plugins {
-    id ("com.android.application") version "7.4.0" apply false
-    id ("com.android.library") version "7.4.0" apply false
+    id ("com.android.application") version "8.1.0" apply false
+    id ("com.android.library") version "8.1.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id ("com.google.dagger.hilt.android") version "2.44" apply false
     id ("org.jetbrains.kotlin.kapt") version "1.8.0"
